92063d3dad7593b602456f40bae4c29d5c82e8b6
Evan P Mills <43226824+epmills@users.noreply.github.com>
2019-10-26 13:38:52 -0400

    Update Homebrew Formula for v1.10

--- Formula/redix.rb
+++ Formula/redix.rb
@@ -1,8 +1,8 @@
 class Redix < Formula
   desc "a persistent real-time key-value store, with the same redis protocol with powerful features"
   homepage "https://alash3al.github.io/redix/"
-  url "https://github.com/alash3al/redix/releases/download/v1.6/redix_darwin_amd64.zip"
-  sha256 "2ee32559f97f57e3274d7fcbdf2a580925b5c6e826d241f545c1799ba88d31fc"
+  url "https://github.com/alash3al/redix/releases/download/v1.10/redix_darwin_amd64.zip"
+  sha256 "32511da3c6642aa2e22c178a50d90b6396f24ebeeb6867be45816d7328ad3e3d"
 
   def install
     # Rename redix_darwin_amd64 To Redix
