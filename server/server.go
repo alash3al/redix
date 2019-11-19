126b84a1503e452e0fc404b2a1b59e92270f85b6
Mohamed Al Ashaal <m7medalash3al@gmail.com>
2019-11-19 20:23:44 +0200

    updated gitignore

--- server/server.go
+++ server/server.go
@@ -16,43 +16,31 @@ type Options struct {
 
 // ListenAndServe start listening and serving the incomming requests
 func ListenAndServe(opts Options) error {
-	return redcon.ListenAndServe(
-		opts.RESPAddr,
-		func(incommingConn redcon.Conn, incommingCommand redcon.Command) {
-			if len(incommingCommand.Args) < 1 {
-				incommingConn.WriteError(errNoCommand.Error())
-				return
-			}
+	return redcon.ListenAndServe(opts.RESPAddr, func(incommingConn redcon.Conn, incommingCommand redcon.Command) {
+		if len(incommingCommand.Args) < 1 {
+			incommingConn.WriteError(errNoCommand.Error())
+			return
+		}
 
-			commandName := strings.ToLower(string(incommingCommand.Args[0]))
-			handler, ok := Handlers[commandName]
+		commandName := strings.ToLower(string(incommingCommand.Args[0]))
+		handler, ok := Handlers[commandName]
 
-			if !ok {
-				incommingConn.WriteError(errUnknownCommand.Error())
-				return
-			}
+		if !ok {
+			incommingConn.WriteError(errUnknownCommand.Error())
+			return
+		}
 
-			db := &db.DB{}
+		db := &db.DB{}
 
-			c := Context{
-				conn:    incommingConn,
-				command: incommingCommand,
-				db:      db,
-			}
+		c := Context{
+			conn:    incommingConn,
+			command: incommingCommand,
+			db:      db,
+		}
 
-			if err := handler.Func(c); err != nil {
-				incommingConn.WriteError(err.Error())
-				return
-			}
-		},
-		func(conn redcon.Conn) bool {
-			// use this function to accept or deny the connection.
-			// log.Printf("accept: %s", conn.RemoteAddr())
-			return true
-		},
-		func(conn redcon.Conn, err error) {
-			// this is called when the connection has been closed
-			// log.Printf("closed: %s, err: %v", conn.RemoteAddr(), err)
-		},
-	)
+		if err := handler.Func(c); err != nil {
+			incommingConn.WriteError(err.Error())
+			return
+		}
+	}, nil, nil)
 }
